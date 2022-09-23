ac7d96da9f8c8fa9a0b457697bc20e37ef6815aa
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-09-23 19:45:48 -0300

    Realizando Req HTTP

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog';
 import logoSvg from '../../assets/img/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export const Header = () => {
   return (
@@ -13,13 +14,7 @@ export const Header = () => {
                 <NewTransactionButton>Nova Transação</NewTransactionButton>
               </Dialog.Trigger>
             
-            <Dialog.Portal>
-              <Dialog.Overlay/>
-              <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-                <Dialog.Close/>
-              </Dialog.Content>
-            </Dialog.Portal>
+              <NewTransactionModal/>
             </Dialog.Root>
         </HeaderContent>
     </HeaderContainer>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,7 +4,31 @@ import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
 import { PriceHighLight, TransactionContainer, TransactionTable } from './styles'
 
+
+interface Transaction{
+    id:number;
+    description:string;
+    category: string;
+    type:'income' | 'outcome';
+    price:number;
+    createdAt:string;
+}
+
 export const Transactions = () => {
+
+    const [transactions,setTransactions] = React.useState<Transaction[]>([]);
+
+    async function req() {
+        const RESPONSE = await fetch('http://localhost:3333/transactions');
+        const JSON = await RESPONSE.json();
+        
+        setTransactions(JSON);
+        return JSON;
+    }
+
+    React.useEffect( () =>{
+       req()
+    } , [])
   return (
     <div>
         <Header/>
@@ -13,6 +37,14 @@ export const Transactions = () => {
             <SearchForm/>
         <TransactionTable>
             <tbody>
+                {transactions.map(transaction => (
+                    <tr>
+                        <td width='50%'>{transaction.description}</td>
+                        <td><PriceHighLight variant={transaction.type}>R$ {transaction.price}</PriceHighLight></td>
+                        <td>{transaction.category}</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                ))}
                 <tr>
                     <td width='50%'>Desenvolvimento de site</td>
                     <td><PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight></td>
